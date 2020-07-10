93fcd201544e3d44102e1d19058320a35bf623cd
ashurealsingh <47302099+ashurealsingh@users.noreply.github.com>
2020-07-11 03:36:43 +0530

    Add files via upload

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+package com.apple.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	System.out.println("This project belongs to Ashutosh");
+    }
+}
